9e7f2dd36ad854ac8d33684339b0a755e94ded8e
alienard <alienard@student.42.fr>
2022-05-24 16:59:23 +0200

    ts error

--- components/section.tsx
+++ components/section.tsx
@@ -1,24 +1,47 @@
-import Link from 'next/link'
+import Link, { LinkProps } from 'next/link'
 import Date from '../components/date'
 import utilStyles from '../styles/utils.module.css'
+import { Menu, Transition } from '@headlessui/react'
+import { forwardRef } from 'react'
+
+// trying to implement https://headlessui.dev/react/menu
+
+const MyLink = forwardRef((props, ref) => {
+  let { href, children, ...rest } = props
+  return (
+    <Link href={href}>
+      <a ref={ref} {...rest}>
+        {children}
+      </a>
+    </Link>
+  )
+})
 
 export default function Section({data, title, dir}){
     return (
-        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>{title}</h2>
-        <ul className={utilStyles.list}>
-          {data.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/${dir}/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <Menu>
+          <Menu.Button>{title}</Menu.Button>
+          <Menu.Items>
+            <ul className={utilStyles.list}>
+              {data.map(({ id, date, title }) => (
+                <li className={utilStyles.listItem} key={id}>
+                  <Menu.Item>
+                    <MyLink href={`/${dir}/${id}`}>{title}</MyLink>
+                    {/* <Link href={`/${dir}/${id}`}>
+                      <a>{title}</a>
+                    </Link> */}
+                    <br />
+                    <small className={utilStyles.lightText}>
+                      <Date dateString={date} />
+                    </small>
+                  </Menu.Item>
+                </li>
+              ))}
+            </ul>
+          </Menu.Items>
+        </Menu>
       </section>
     )
 }
